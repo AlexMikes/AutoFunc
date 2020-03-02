6a3a6071165951a5cb95d2c94cd3eb4dddb7016c
AlexMikes <alexmikes@gmail.com>
2020-03-02 10:54:09 -0800

    Updating tests for JOSS

--- autofunc/tests/test_get_counts.py
+++ autofunc/tests/test_get_counts.py
@@ -4,11 +4,11 @@ from autofunc.make_df import make_df
 
 def test1():
 
-""" Testing the counter function. When using the consumer_systems dataset, housing is the most common component """
+    """ Testing the counter function. When using the consumer_systems dataset, housing is the most common component """
 
     script_dir = os.path.dirname(__file__)
     file1 = os.path.join(script_dir, '../assets/consumer_systems.csv')
-    
+
     df = make_df(file1)
 
     comb_sort, counts, combos = counter_pandas_with_counts(df)
